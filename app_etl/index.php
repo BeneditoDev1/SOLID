697f497dab47ee857b5781c1a09e5e369b8347ea
Benedito Magalhães <bencaceres1799@gmail.com>
2024-04-14 11:58:44 -0300

    Criado o codigo para leitura de arquivos CSV e TXT

---
+++ app_etl/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require __DIR__."/vendor/autoload.php";
+
+use src\Leitor;
+
+$leitor = new Leitor();
+$leitor->setDiretorio(__DIR__.'/arquivos');
+$leitor->setArquivo('dados.csv');
+
+echo '<pre>';
+print_r($leitor->lerArquivo());
+echo '</pre>';
+
+$leitor = new Leitor();
+$leitor->setDiretorio(__DIR__.'/arquivos');
+$leitor->setArquivo('dados.txt');
+echo '<pre>';
+print_r($leitor->lerArquivo());
+echo '</pre>';
