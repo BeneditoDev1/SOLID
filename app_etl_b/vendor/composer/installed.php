268dc7fbf649b963725b93dcbba34fef827f5a42
Benedito Magalhães <bencaceres1799@gmail.com>
2024-04-14 12:09:27 -0300

    Atualizacao carrinho de compras_B e criacao de projeto app_etl com OCP

---
+++ app_etl_b/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'benedito/app_etl',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '902c164f931095e28ce4f63bbe69a1273bba4783',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'benedito/app_etl' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '902c164f931095e28ce4f63bbe69a1273bba4783',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
